79cf7193edefe3e74ee78d8592ba13c57addffa7
imumble <57304656+imumble@users.noreply.github.com>
2023-12-09 23:12:58 -0600

    Baby added
    Left and right animation added for Milk Baby

--- Scripts/Main.cs
+++ Scripts/Main.cs
@@ -3,46 +3,46 @@ using System;
 
 public partial class Main : Node
 {
-    [Export]
-    public PackedScene MobScene { get; set; }
+	[Export]
+	public PackedScene MobScene { get; set; }
 
-    public int Score;
-    public void GameOver()
-    {
-        Timer scoreTimer = GetNodeOrNull<Timer>("ScoreTimer");
-        if (scoreTimer != null)
-        {
-            scoreTimer.Stop();
-        }
+	public int Score;
+	public void GameOver()
+	{
+		Timer scoreTimer = GetNodeOrNull<Timer>("ScoreTimer");
+		if (scoreTimer != null)
+		{
+			scoreTimer.Stop();
+		}
 
-        GetNode<HUD>("HUD").ShowGameOver();
-    }
+		GetNode<HUD>("HUD").ShowGameOver();
+	}
 
 
-    public void NewGame()
-    {
-        GetTree().CallGroup("enemy", Node.MethodName.QueueFree);
-        GetTree().CallGroup("pickup", Node.MethodName.QueueFree);
-        Score = 0;
+	public void NewGame()
+	{
+		GetTree().CallGroup("enemy", Node.MethodName.QueueFree);
+		GetTree().CallGroup("pickup", Node.MethodName.QueueFree);
+		Score = 0;
 
-        Player player = GetNode<Player>("Player");
-        Marker2D startPosition = GetNode<Marker2D>("StartPosition");
-        player.Start(startPosition.Position);
-        player.GetNode<Health>("Health").CurrentHealth = player.GetNode<Health>("Health").MaxHealth;
+		Player player = GetNode<Player>("Player");
+		Marker2D startPosition = GetNode<Marker2D>("StartPosition");
+		player.Start(startPosition.Position);
+		player.GetNode<Health>("Health").CurrentHealth = player.GetNode<Health>("Health").MaxHealth;
 
-        GetNode<Timer>("StartTimer").Start();
-        HUD hud = GetNode<HUD>("HUD");
-        hud.UpdateScore(Score);
-        hud.ShowMessage("Get Ready!");
-    }
-    private void OnScoreTimerTimeout()
-    {
-        Score++;
-        GetNode<HUD>("HUD").UpdateScore(Score);
-    }
+		GetNode<Timer>("StartTimer").Start();
+		HUD hud = GetNode<HUD>("HUD");
+		hud.UpdateScore(Score);
+		hud.ShowMessage("Get Ready!");
+	}
+	private void OnScoreTimerTimeout()
+	{
+		Score++;
+		GetNode<HUD>("HUD").UpdateScore(Score);
+	}
 
-    private void OnStartTimerTimeout()
-    {
-        GetNode<Timer>("ScoreTimer").Start();
-    }
+	private void OnStartTimerTimeout()
+	{
+		GetNode<Timer>("ScoreTimer").Start();
+	}
 }
--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -4,90 +4,90 @@ using System;
 public partial class Player : Area2D
 {
 
-    [Signal]
-    public delegate void HitEventHandler();
+	[Signal]
+	public delegate void HitEventHandler();
 
 
-    [Export]
-    public int Speed { get; set; } = 400; // How fast the player will move (pixels/sec).
-    public bool IsAlive { get; set; } = false;
+	[Export]
+	public int Speed { get; set; } = 400; // How fast the player will move (pixels/sec).
+	public bool IsAlive { get; set; } = false;
 
-    //public Vector2 ScreenSize; // Size of the game window.
-    public override void _Ready()
+	//public Vector2 ScreenSize; // Size of the game window.
+	public override void _Ready()
 	{
-       // ScreenSize = GetViewportRect().Size;
-        Hide();
-    }
+	   // ScreenSize = GetViewportRect().Size;
+		Hide();
+	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-        if (!IsInstanceValid(this))
-            return;
+		if (!IsInstanceValid(this))
+			return;
 
-        LookAt(GetGlobalMousePosition());
-        Vector2 velocity = Vector2.Zero; // The player's movement vector.
+		LookAt(GetGlobalMousePosition());
+		Vector2 velocity = Vector2.Zero; // The player's movement vector.
 
-        if (Input.IsActionPressed("ui_right"))
-        {
-            velocity.X += 1;
-        }
+		if (Input.IsActionPressed("ui_right"))
+		{
+			velocity.X += 1;
+		}
 
-        if (Input.IsActionPressed("ui_left"))
-        {
-            velocity.X -= 1;
-        }
+		if (Input.IsActionPressed("ui_left"))
+		{
+			velocity.X -= 1;
+		}
 
-        if (Input.IsActionPressed("ui_down"))
-        {
-            velocity.Y += 1;
-        }
+		if (Input.IsActionPressed("ui_down"))
+		{
+			velocity.Y += 1;
+		}
 
-        if (Input.IsActionPressed("ui_up"))
-        {
-            velocity.Y -= 1;
-        }
+		if (Input.IsActionPressed("ui_up"))
+		{
+			velocity.Y -= 1;
+		}
 
-        AnimatedSprite2D animatedSprite2D = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+		AnimatedSprite2D animatedSprite2D = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
 
-        if (velocity.Length() > 0)
-        {
-            velocity = velocity.Normalized() * Speed;
-            animatedSprite2D.Play();
-        }
-        else
-        {
-            animatedSprite2D.Stop();
-        }
-        Position += velocity * (float)delta;
-        //Position = new Vector2(
-        //    x: Mathf.Clamp(Position.X, 0, ScreenSize.X),
-        //    y: Mathf.Clamp(Position.Y, 0, ScreenSize.Y)
-        //);
-        if (velocity.X != 0)
-        {
-            animatedSprite2D.Animation = "walk";
-            animatedSprite2D.FlipV = false;
-            // See the note below about boolean assignment.
-            animatedSprite2D.FlipH = velocity.X < 0;
-        }
-        else if (velocity.Y != 0)
-        {
-            animatedSprite2D.Animation = "up";
-            animatedSprite2D.FlipV = velocity.Y > 0;
-        }
-    }
-    public void Start(Vector2 position)
-    {
-        Position = position;
-        Show();
-        IsAlive = true;
-    }
+		if (velocity.Length() > 0)
+		{
+			velocity = velocity.Normalized() * Speed;
+			animatedSprite2D.Play();
+		}
+		else
+		{
+			animatedSprite2D.Stop();
+		}
+		Position += velocity * (float)delta;
+		//Position = new Vector2(
+		//    x: Mathf.Clamp(Position.X, 0, ScreenSize.X),
+		//    y: Mathf.Clamp(Position.Y, 0, ScreenSize.Y)
+		//);
+		if (velocity.X != 0)
+		{
+			animatedSprite2D.Animation = "walk";
+			animatedSprite2D.FlipV = false;
+			// See the note below about boolean assignment.
+			animatedSprite2D.FlipH = velocity.X < 0;
+		}
+		else if (velocity.Y != 0)
+		{
+			animatedSprite2D.Animation = "up";
+			animatedSprite2D.FlipV = velocity.Y > 0;
+		}
+	}
+	public void Start(Vector2 position)
+	{
+		Position = position;
+		Show();
+		IsAlive = true;
+	}
 
-    public void Die()
-    {
-        IsAlive = false;
-        EmitSignal(SignalName.Hit);
-        Hide();
-    }
+	public void Die()
+	{
+		IsAlive = false;
+		EmitSignal(SignalName.Hit);
+		Hide();
+	}
 }
