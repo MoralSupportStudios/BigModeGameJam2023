ea8c6fc7d89ac53dfaf2cd995e6deffe118e6d20
imumble <57304656+imumble@users.noreply.github.com>
2023-12-12 22:31:25 -0600

    Added Knome animation
    Knome is now the enemy. Just needs code to trigger the animation.

--- Scripts/Boomerang.cs
+++ Scripts/Boomerang.cs
@@ -3,55 +3,55 @@ using System;
 
 public partial class Boomerang : Area2D
 {
-    [Export] public float damage = 2f;
-    [Export] public float orbitSpeed = 300f; // Speed at which the boomerang orbits
-    [Export] public float returnSpeed = 200f; // Speed at which the boomerang returns
-    [Export] public float orbitRadius = 100f; // Radius of the orbit
+	[Export] public float damage = 2f;
+	[Export] public float orbitSpeed = 300f; // Speed at which the boomerang orbits
+	[Export] public float returnSpeed = 200f; // Speed at which the boomerang returns
+	[Export] public float orbitRadius = 100f; // Radius of the orbit
 
-    public Vector2 orbitCenter;
-    private float orbitAngle = 0f;
-    private const float THREE_QUARTERS_TAU = Mathf.Pi * 1.5f; // Represents three-quarters of a full rotation
-    private bool isReturning = false;
+	public Vector2 orbitCenter;
+	private float orbitAngle = 0f;
+	private const float THREE_QUARTERS_TAU = Mathf.Pi * 1.5f; // Represents three-quarters of a full rotation
+	private bool isReturning = false;
 
-    public override void _Ready()
-    {
-        //orbitCenter = GetTree().Root.GetNode("Main").GetNode("Player").GetNode<Node2D>("Gun").GlobalPosition; // Set the orbit center to the gun's position
-    }
+	public override void _Ready()
+	{
+		//orbitCenter = GetTree().Root.GetNode("Main").GetNode("Player").GetNode<Node2D>("Gun").GlobalPosition; // Set the orbit center to the gun's position
+	}
 
-    public override void _Process(double delta)
-    {
-        if (!isReturning)
-        {
-            orbitAngle += orbitSpeed * (float)delta / orbitRadius;
+	public override void _Process(double delta)
+	{
+		if (!isReturning)
+		{
+			orbitAngle += orbitSpeed * (float)delta / orbitRadius;
 
-            Vector2 offset = new Vector2(Mathf.Sin(orbitAngle), Mathf.Cos(orbitAngle)) * orbitRadius;
-            GlobalPosition = orbitCenter + offset;
+			Vector2 offset = new Vector2(Mathf.Sin(orbitAngle), Mathf.Cos(orbitAngle)) * orbitRadius;
+			GlobalPosition = orbitCenter + offset;
 
-            if (orbitAngle >= THREE_QUARTERS_TAU)
-            {
-                isReturning = true; // Signal that the Boomerang should start returning
-            }
-        }
-        else
-        {
-            // Return to the gun's position
-            Vector2 directionToGun = (orbitCenter - GlobalPosition).Normalized();
-            GlobalPosition += directionToGun * returnSpeed * (float)delta;
+			if (orbitAngle >= THREE_QUARTERS_TAU)
+			{
+				isReturning = true; // Signal that the Boomerang should start returning
+			}
+		}
+		else
+		{
+			// Return to the gun's position
+			Vector2 directionToGun = (orbitCenter - GlobalPosition).Normalized();
+			GlobalPosition += directionToGun * returnSpeed * (float)delta;
 
-            // Check if the Boomerang has reached the Gun's position (within a small threshold)
-            if (GlobalPosition.DistanceTo(orbitCenter) < returnSpeed * (float)delta)
-            {
-                QueueFree(); // Remove the Boomerang from the scene once it has returned
-            }
-        }
-    }
+			// Check if the Boomerang has reached the Gun's position (within a small threshold)
+			if (GlobalPosition.DistanceTo(orbitCenter) < returnSpeed * (float)delta)
+			{
+				QueueFree(); // Remove the Boomerang from the scene once it has returned
+			}
+		}
+	}
 
-    private void OnBodyEntered(Node2D body)
-    {
-        if (body.IsInGroup("enemy"))
-        {
-            Enemy enemy = (Enemy)body;
-            enemy.GetNode<Health>("Health").Damage(damage);
-        }
-    }
+	private void OnBodyEntered(Node2D body)
+	{
+		if (body.IsInGroup("enemy"))
+		{
+			Enemy enemy = (Enemy)body;
+			enemy.GetNode<Health>("Health").Damage(damage);
+		}
+	}
 }
--- Scripts/HUD.cs
+++ Scripts/HUD.cs
@@ -3,70 +3,70 @@ using System;
 
 public partial class HUD : CanvasLayer
 {
-    [Signal]
-    public delegate void StartGameEventHandler();
+	[Signal]
+	public delegate void StartGameEventHandler();
 
-    private ProgressBar healthBar; // This will be our health bar on the HUD.
-    private Health playerHealth; // This will hold a reference to the player's health.
+	private ProgressBar healthBar; // This will be our health bar on the HUD.
+	private Health playerHealth; // This will hold a reference to the player's health.
 
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
 	{
-        healthBar = GetNode<ProgressBar>("HealthBar"); // Correctly assign the node path for your health bar.
-        playerHealth = GetParent().GetNode<Health>("Player/Health"); // Correctly assign the node path to your player's Health node.
-        healthBar.MaxValue = playerHealth.MaxHealth;
-        healthBar.Value = playerHealth.CurrentHealth;
-    }
+		healthBar = GetNode<ProgressBar>("HealthBar"); // Correctly assign the node path for your health bar.
+		playerHealth = GetParent().GetNode<Health>("Player/Health"); // Correctly assign the node path to your player's Health node.
+		healthBar.MaxValue = playerHealth.MaxHealth;
+		healthBar.Value = playerHealth.CurrentHealth;
+	}
 
-    // Called every frame. 'delta' is the elapsed time since the previous frame.
-    public override void _Process(double delta)
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
 	{
-        UpdateHealthBar();
-    }
-    private void UpdateHealthBar()
-    {
-        if (playerHealth != null && healthBar != null)
-        {
-            healthBar.Value = playerHealth.CurrentHealth;
-        }
-    }
+		UpdateHealthBar();
+	}
+	private void UpdateHealthBar()
+	{
+		if (playerHealth != null && healthBar != null)
+		{
+			healthBar.Value = playerHealth.CurrentHealth;
+		}
+	}
 
 
 
-    public void ShowMessage(string text)
-    {
-        Label message = GetNode<Label>("Message");
-        message.Text = text;
-        message.Show();
+	public void ShowMessage(string text)
+	{
+		Label message = GetNode<Label>("Message");
+		message.Text = text;
+		message.Show();
 
-        GetNode<Timer>("MessageTimer").Start();
-    }
-    async public void ShowGameOver(int score)
-    {
-        ShowMessage("Final Score = " + score);
+		GetNode<Timer>("MessageTimer").Start();
+	}
+	async public void ShowGameOver(int score)
+	{
+		ShowMessage("Final Score = " + score);
 
-        Timer messageTimer = GetNode<Timer>("MessageTimer");
-        await ToSignal(messageTimer, Timer.SignalName.Timeout);
+		Timer messageTimer = GetNode<Timer>("MessageTimer");
+		await ToSignal(messageTimer, Timer.SignalName.Timeout);
 
-        Label message = GetNode<Label>("Message");
-        message.Text = "Shoot with Left Click! Move with WASD or Arrow Keys";
-        message.Show();
+		Label message = GetNode<Label>("Message");
+		message.Text = "Shoot with Left Click! Move with WASD or Arrow Keys";
+		message.Show();
 
-        await ToSignal(GetTree().CreateTimer(1.0), SceneTreeTimer.SignalName.Timeout);
-        GetNode<Button>("StartButton").Show();
-    }
-    public void UpdateScore(int score)
-    {
-        GetNode<Label>("ScoreLabel").Text = "Score: " + score.ToString();
-    }
-    private void OnStartButtonPressed()
-    {
-        GetNode<Button>("StartButton").Hide();
-        EmitSignal(SignalName.StartGame);
-    }
+		await ToSignal(GetTree().CreateTimer(1.0), SceneTreeTimer.SignalName.Timeout);
+		GetNode<Button>("StartButton").Show();
+	}
+	public void UpdateScore(int score)
+	{
+		GetNode<Label>("ScoreLabel").Text = "Score: " + score.ToString();
+	}
+	private void OnStartButtonPressed()
+	{
+		GetNode<Button>("StartButton").Hide();
+		EmitSignal(SignalName.StartGame);
+	}
 
-    private void OnMessageTimerTimeout()
-    {
-        GetNode<Label>("Message").Hide();
-    }
+	private void OnMessageTimerTimeout()
+	{
+		GetNode<Label>("Message").Hide();
+	}
 }
